8d10d8300d39f873835c5789c6d62e648bd5dca2
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-07-13 18:42:26 +0300

    Add QueryProvider and SheetProvider components to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import QueryProvider from '~/providers/query-provider';
+import { SheetProvider } from '~/providers/sheet-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +20,12 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <QueryProvider>
+            <SheetProvider />
+            {children}
+          </QueryProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
