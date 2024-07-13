71cc05c1fad6aea2226531bb390e474b8f6ac302
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-07-13 18:41:48 +0300

    Add client-side logic for creating new accounts
    - Import Button component and useNewAccount hook
    - Implement functionality to open new account creation form on button click

--- app/(dashbord)/page.tsx
+++ app/(dashbord)/page.tsx
@@ -1,5 +1,11 @@
+'use client';
+
+import { Button } from '~/components/ui/button';
+import useNewAccount from '~/features/accounts/hooks/use-new-account';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { onOpen } = useNewAccount();
+  return <Button onClick={onOpen}>Create a new account</Button>;
 };
 
 export default Dashboard;
