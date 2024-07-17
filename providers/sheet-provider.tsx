3df76e534666d880e482e0bd07953ef5ca3f53be
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-07-17 18:35:04 +0300

    Update schema with new relations and insert schemas

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -5,6 +5,8 @@ import EditAccountSheet from '~/features/accounts/components/edit-account-sheet'
 import NewAccountSheet from '~/features/accounts/components/new-account-sheet';
 import EditCategorySheet from '~/features/categories/components/edit-category-sheet';
 import NewCategorySheet from '~/features/categories/components/new-category-sheet';
+import EditTransactionSheet from '~/features/transactions/components/edit-transaction-sheet';
+import NewTransactionSheet from '~/features/transactions/components/new-transaction-sheet';
 
 export const SheetProvider = () => {
   const isMounted = useMountedState();
@@ -19,6 +21,8 @@ export const SheetProvider = () => {
       <EditAccountSheet />
       <EditCategorySheet />
       <NewCategorySheet />
+      <NewTransactionSheet />
+      <EditTransactionSheet />
     </>
   );
 };
